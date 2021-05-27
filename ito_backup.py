67dd9432fcc15a51320d51c36742ab132b3e48bb
Jimmature <jimstorch@gmail.com>
2021-05-27 08:59:19 -0400

    Python3 format strings + Python Logging library

--- ito_backup.py
+++ ito_backup.py
@@ -11,6 +11,7 @@ Backups are rotated and hard links are used to save space for unchanged files.
 import configparser
 import datetime
 import locale
+import logging
 import os
 import platform
 import re
@@ -19,13 +20,6 @@ import smtplib
 import subprocess
 import sys
 from email.mime.text import MIMEText
-from time import strftime
-
-locale.setlocale(locale.LC_ALL, 'en_US.utf8')
-
-
-# Regular expression to extract the backup byte count
-SIZE_RE = re.compile(r"""total size is (\d+)""", re.MULTILINE)
 
 
 class RsyncError(Exception):
@@ -33,19 +27,14 @@ class RsyncError(Exception):
     pass
 
 
-def _timestamp():
-    """Return the current date and time in a log friendly format."""
-    return datetime.datetime.now().strftime("%Y-%m-%d-%H%M.%S")
-
-
 def _mb(value):
     """Convert value to human readable megabytes."""
-    return '%sMB' % locale.format_string('%.2f', value / 1048576.0, True)
+    locale.setlocale(locale.LC_ALL, 'en_US.utf8')
+    return f"{locale.format_string(' % .2f', value / 1048576.0, True)}MB"
+
 
 def _email_log(config, logfile_name, happy):
-    #
-    #   Email Report
-    #
+    """Email the log for this backup."""
     smtp_enable = config.get('General', 'smtp_enable')
     smtp_server = config.get('General', 'smtp_server')
     smtp_email = config.get('General', 'smtp_email')
@@ -59,14 +48,41 @@ def _email_log(config, logfile_name, happy):
         mime = MIMEText(msg)
         mime['From'] = smtp_email
         if happy:
-            mime['Subject'] = "Backup complete on %s" % thismachine
+            mime['Subject'] = f'Backup complete on {thismachine}'
         else:
-            mime['Subject'] = "BACKUP FAILED ON %s" % thismachine
+            mime['Subject'] = f'BACKUP FAILED ON {thismachine}'
         mime['To'] = smtp_recipients
         server = smtplib.SMTP(smtp_server)
         server.login(smtp_email, smtp_password)
         server.sendmail(mime['From'], mime['To'].split(','), mime.as_string())
         server.quit()
+
+
+def _timestamp():
+    """Return the current date and time in a log friendly format."""
+    return datetime.datetime.now().strftime("%Y-%m-%d-%H%M.%S")
+
+
+# Regular expression to extract the backup byte count
+SIZE_RE = re.compile(r"""total size is (\d+)""", re.MULTILINE)
+
+LOG_LEVEL = logging.DEBUG
+log = logging.getLogger()
+log.setLevel(LOG_LEVEL)
+logfile_name = os.path.join('logs', f'backup-{_timestamp()}.log')
+
+file = logging.FileHandler(logfile_name)
+fileformat = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+file.setLevel(LOG_LEVEL)
+file.setFormatter(fileformat)
+
+console = logging.StreamHandler()
+streamformat = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+console.setLevel(logging.DEBUG)
+console.setFormatter(streamformat)
+
+log.addHandler(file)
+log.addHandler(console)
 
 
 if __name__ == '__main__':
@@ -80,17 +96,6 @@ if __name__ == '__main__':
     config.read('config.ini')
 
     #
-    #  Logging
-    #
-    log_folder = config.get('General', 'log_folder')
-    logfile_name = os.path.join(log_folder, 'backup-%s.log' % _timestamp())
-    logfp = open(logfile_name, 'wt')
-
-    def _log(text):
-        logfp.write('%s  %s\n' % (_timestamp(), text))
-    _log('batch backup started on device "%s"' % thismachine)
-
-    #
     #  Batch Backup
     #
     backup_folder = config.get('General', 'backup_folder')
@@ -100,8 +105,7 @@ if __name__ == '__main__':
     #
     if config.get('General', 'mount_check').lower() == 'true':
         if not os.path.ismount(backup_folder):
-            _log('!! backup point %s not mounted' % backup_folder)
-            logfp.close()
+            log.error(f'!! backup point {backup_folder} not mounted')
             _email_log(config, logfile_name, False)
             sys.exit(1)
 
@@ -111,21 +115,22 @@ if __name__ == '__main__':
     for job in config.sections():
         if job == 'General':
             continue
-        _log('job "%s" begun' % job)
+        log.info(f'*** Job "{job}" begun ***')
         host = config.get(job, 'host')
         username = config.get(job, 'username')
         password = config.get(job, 'password')
         rotate_level = int(config.get(job, 'rotate_level'))
-        rsync_server = '%s@%s::' % (username, host.strip())
+        rsync_server = f'{username}@{host.strip()}::'
 
         #
         #   Ask the RSYNC/Delta Copy Server what virtual directories it serves.
         #
-        _log('.. requesting rsync targets from %s' % host)
+        log.info('Requesting rsync targets from %s' % host)
         try:
             cmd = ['rsync', rsync_server]
-            process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env={
-                                       'RSYNC_PASSWORD': password})
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE,
+                                       env={'RSYNC_PASSWORD': password})
             stdout, stderr = process.communicate()
             status = process.returncode
             if status != 0:
@@ -135,7 +140,7 @@ if __name__ == '__main__':
         except Exception as err:
             happy = False
             err_msg = str(err)
-            _log('!! directory listing error: %s' % err_msg)
+            log.error(f'Directory listing error: {err_msg}')
             continue
 
         subtotal = 0
@@ -145,7 +150,7 @@ if __name__ == '__main__':
             print(folder)
             target_path = os.path.join(backup_folder, job)
             source = rsync_server + os.path.join(folder) + '/'
-            _log('.. syncing "%s" to %s' % (folder, target_path))
+            log.info(f'Syncing "{folder}" to {target_path}')
 
             try:
 
@@ -154,16 +159,18 @@ if __name__ == '__main__':
                 #   create a subfolder named after job.
                 #
                 if not os.path.isdir(backup_folder):
-                    raise RsyncError('!! backup_folder does not exist: %s ')
+                    raise RsyncError(
+                        f'!! backup_folder does not exist: {backup_folder}')
                 if not os.path.isdir(target_path):
                     os.makedirs(target_path)
 
                 #
                 #   Purge backup sets older than rotate_level.
-                #   We overshoot to 99 in case the rotation is lowered at a future date
+                #   We overshoot to 99 in case the rotation is lowered at a
+                #   future date
                 #
                 for r in range(rotate_level-1, 99):
-                    last_rot = os.path.join(target_path, '%s.%d' % (folder, r))
+                    last_rot = os.path.join(target_path, f'{folder}.{r}')
                     if os.path.isdir(last_rot):
                         shutil.rmtree(last_rot)
 
@@ -171,16 +178,16 @@ if __name__ == '__main__':
                 #   Rotate the incremental backup folders upward, if they exist
                 #
                 for x in range(rotate_level - 2, -1, -1):
-                    rot_from = os.path.join(target_path, '%s.%d' % (folder, x))
+                    rot_from = os.path.join(target_path, f'{folder}.{x}')
                     rot_to = os.path.join(
-                        target_path, '%s.%d' % (folder, x + 1))
+                        target_path, f'{folder}.{x+1}')
                     if os.path.isdir(rot_from):
                         os.rename(rot_from, rot_to)
 
                 #
                 #   Create the new rsync target folder.0
                 #
-                zero_folder_path = os.path.join(target_path, '%s.0' % folder)
+                zero_folder_path = os.path.join(target_path, f'{folder}.0')
                 os.mkdir(zero_folder_path)
 
                 #
@@ -190,17 +197,18 @@ if __name__ == '__main__':
                          '--no-super', '--no-group', '--no-human-readable']
 
                 #
-                #   If we have last night's backup, pass it as rsync's link destination
-                #   to preserve disk space
+                #   If we have last night's backup, pass it as rsync's link
+                #   destination to preserve disk space
                 #
-                link_src = os.path.join(target_path, '%s.1' % folder)
+                link_src = os.path.join(target_path, f'{folder}.1')
                 if os.path.isdir(link_src):
-                    parms.append('--link-dest=%s' % link_src)
+                    parms.append(f'--link-dest={link_src}')
                 parms.append(source)
                 parms.append(zero_folder_path)
                 cmd = ['rsync']
                 cmd += parms
-                process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                           stderr=subprocess.PIPE,
                                            env={"RSYNC_PASSWORD": password})
                 stdout, stderr = process.communicate()
                 status = process.returncode
@@ -210,22 +218,17 @@ if __name__ == '__main__':
                     match_obj = SIZE_RE.search(stdout.decode('utf-8'))
                     size = int(match_obj.group(1))
                     subtotal += size
-                    _log('.. synchronized %s' % _mb(size))
+                    log.info(f'Synchronized {_mb(size)}')
 
             except Exception as err:
                 happy = False
                 err_msg = str(err)
-                _log('!! error: %s' % err_msg)
+                log.error(f'!! Error: {err_msg}')
 
-
-        _log('.. job "%s" complete with %s processed' % (job, _mb(subtotal)))
+        log.info(f'Job {job} complete with {_mb(subtotal)} processed')
         total += subtotal
 
-    _log('batch backup finished on device "%s", total size was %s' %
-         (thismachine, _mb(total)))
-    logfp.close()
-
+    log.info(
+        f'Batch backup finished on "{thismachine}". '
+        f'Total size was {_mb(total)}')
     _email_log(config, logfile_name, happy)
-
-
-
